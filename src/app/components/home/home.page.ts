edcb1beea81f4dc4fd9f5c9e6018e1235e5d0944
Syahrul (/#) <slashsharp@gmail.com>
2025-04-18 02:00:23 +0800

    Update home page design
    Update tabs color, global color and floating button

--- src/app/components/home/home.page.ts
+++ src/app/components/home/home.page.ts
@@ -1,15 +1,21 @@
 import { Component } from '@angular/core';
 import { IonContent } from '@ionic/angular/standalone';
 import { DateFormatPipe } from 'src/app/pipes/date-format.pipe';
+import { IonFab, IonFabButton, IonIcon } from '@ionic/angular/standalone';
+import { addIcons } from 'ionicons';
+import { addSharp } from 'ionicons/icons';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonContent, DateFormatPipe],
+  imports: [IonContent, DateFormatPipe, IonFab, IonFabButton, IonIcon],
 })
 export class Tab1Page {
+  // Display date on top of the page
   date = new Date();
 
-  constructor() { }
+  constructor() {
+    addIcons({ addSharp });
+  }
 }
